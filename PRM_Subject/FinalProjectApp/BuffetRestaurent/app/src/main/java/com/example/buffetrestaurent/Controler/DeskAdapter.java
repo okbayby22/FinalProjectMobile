5afd682a7c7ef22f8026d71a18d4885f9d63f24f
Hai Long <hailong13700@gmail.com>
2021-06-18 17:25:44 +0700

    [LongNNHH}

--- PRM_Subject/FinalProjectApp/BuffetRestaurent/app/src/main/java/com/example/buffetrestaurent/Controler/DeskAdapter.java
+++ PRM_Subject/FinalProjectApp/BuffetRestaurent/app/src/main/java/com/example/buffetrestaurent/Controler/DeskAdapter.java
@@ -1,99 +1,47 @@
 package com.example.buffetrestaurent.Controler;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.buffetrestaurent.Model.Desk;
+import com.example.buffetrestaurent.Model.Food;
 import com.example.buffetrestaurent.R;
 
-import java.util.ArrayList;
+import java.util.List;
 
 
-public class DeskAdapter extends RecyclerView.Adapter<DeskAdapter.ViewHolder> {
+public class DeskAdapter extends RecyclerView.Adapter<DeskAdapter.MyViewHolder> {
 
-    ArrayList<Desk> desksList;
-
-
-    Context context;
-
-    /**
-     * Constructor method Teacher adapter
-     *
-     * @param desksList: stores the teacher list
-     * @param context      : stores the context
-     */
-    public DeskAdapter(ArrayList desksList, Context context) {
-        this.desksList = desksList;
-        this.context = context;
+    private List<Desk> desklist;
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView txtFoodName;
+        MyViewHolder(View view) {
+            super(view);
+            txtFoodName = view.findViewById(R.id.listfood_txtFoodName);
+        }
     }
-    /**
-     * Method onBindViewHolder : move the data into the ViewHolder
-     *
-     * @param holder   :stores the ViewHolder
-     * @param position :stores the position of the object when binding data into the view
-     */
+    public DeskAdapter(List<Desk> deskList) {
+        this.desklist = deskList;
+    }
+    @NonNull
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Desk desk = desksList.get(position);
-        holder.deskName.setText(desk.getDeskId());
-     //   new ImageURL(holder.itemView.findViewById(R.id.imageView2)).execute(teacher.getUrlImage());
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_layout, parent, false);
+        return new MyViewHolder(itemView);
     }
-
-    /**
-     * Method onCreateViewHolder : create ViewHolder object and store the view for showing data
-     *
-     * @param parent    : stores the parent group containing all views inside
-     * @param viewType: stores the type of view
-     * @return the new ViewHolder after inflating a new view based on xml file
-     */
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
-        LayoutInflater inflater = LayoutInflater.from(context);
-        //View teacherView = inflater.inflate(R.layout.item_layout, parent, false);
-        View teacherView = inflater.inflate(R.layout.item_layout, parent, false);
-        return new ViewHolder(teacherView);
+    public void onBindViewHolder(FoodsAdapter.MyViewHolder holder, int position) {
+        Desk desk = desklist.get(position);
+        holder.txtFoodName.setText(desk.getDeskId());
     }
-
-
-
-    /**
-     * Method getItemCounts:  get the size of the list
-     *
-     * @return the size of the list
-     */
     @Override
     public int getItemCount() {
-        return desksList.size();
-    }
-
-    /**
-     * Create class ViewHolder to keep the View structure
-     */
-    public class ViewHolder extends RecyclerView.ViewHolder {
-
-        // Store the teacher's image
-        public ImageView deskImage;
-
-        //Store the teacher's name
-        public TextView deskName;
-
-        /**
-         * Create constructor method ViewHolder
-         *
-         * @param itemView : stores the view of the activity
-         */
-        public ViewHolder(View itemView) {
-            super(itemView);
-            //deskImage = itemView.findViewById(R.id.de);
-            deskName = itemView.findViewById(R.id.tableName);
-        }
+        return desklist.size();
     }
 }
--- PRM_Subject/FinalProjectApp/BuffetRestaurent/app/src/main/java/com/example/buffetrestaurent/Controler/tableFragement.java
+++ PRM_Subject/FinalProjectApp/BuffetRestaurent/app/src/main/java/com/example/buffetrestaurent/Controler/tableFragement.java
@@ -3,12 +3,19 @@ package com.example.buffetrestaurent.Controler;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.buffetrestaurent.Model.Desk;
 import com.example.buffetrestaurent.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +28,9 @@ public class tableFragement extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    ArrayList <Desk> desksList ;
+    DeskAdapter deskAdapter;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -61,6 +71,16 @@ public class tableFragement extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_table_fragement, container, false);
+        View rootView= inflater.inflate(R.layout.fragment_table_fragement, container, false);
+        RecyclerView recyclerView = rootView.findViewById(R.id.homepage_listFoodList);
+        desksList.add(new Desk(1,1,1));
+        deskAdapter = new DeskAdapter(desksList,);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(rootView.getContext());
+        mLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(fAdapter);
+        prepareFoodData();
+        return rootView;
     }
 }
