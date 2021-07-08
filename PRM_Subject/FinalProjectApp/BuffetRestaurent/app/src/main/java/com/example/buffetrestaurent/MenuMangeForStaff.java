1b7e2142dfbcb9689f962fde37fe5270d8040209
Hai Long <hailong13700@gmail.com>
2021-07-08 21:24:38 +0700

    [LongNNHH]_Update

---
+++ PRM_Subject/FinalProjectApp/BuffetRestaurent/app/src/main/java/com/example/buffetrestaurent/MenuMangeForStaff.java
@@ -0,0 +1,14 @@
+package com.example.buffetrestaurent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuMangeForStaff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_mange_for_staff);
+    }
+}
